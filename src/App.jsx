ea29185a9cb2bb347c7a043bdf4fbd99a37e2058
Paula <paulaoorjuela@gmail.com>
2025-04-09 13:22:10 -0500

    add reusable StarRating component

--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,80 @@ import Navbar from "./Navbar";
 import MainContent from "./MainContent";
 import SearchBar from "./navbar-components/SearchBar";
 import NumResults from "./navbar-components/NumResults";
-import ListBox from "./main-content-components/ListBox";
-import WatchedBox from "./main-content-components/WatchedBox";
 import MovieList from "./main-content-components/list-box-components/MovieList";
+import MovieDisplayBox from "./main-content-components/MovieDisplayBox";
+import WatchedMoviesSummary from "./main-content-components/watched-box-components/WatchedMoviesSummary";
+import WatchedMoviesList from "./main-content-components/watched-box-components/WatchedMoviesList";
+import StarRating from "./StarRating";
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
 
 const tempMovieData = [
   {
-      imdbID: "tt1375666",
-      Title: "Inception",
-      Year: "2010",
-      Poster:
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
   },
   {
-      imdbID: "tt0133093",
-      Title: "The Matrix",
-      Year: "1999",
-      Poster:
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
       "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
   },
   {
-      imdbID: "tt6751668",
-      Title: "Parasite",
-      Year: "2019",
-      Poster:
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
   },
-  ];
+];
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
-  
+  const [watched, setWatched] = useState(tempWatchedData);
+
   return (
     <>
       <Navbar>
-        <SearchBar/>
-        <NumResults movies={movies}/>
+        <SearchBar />
+        <NumResults movies={movies} />
       </Navbar>
       <MainContent>
-        <ListBox>
-          <MovieList movies={movies}/>
-        </ListBox>
-        <WatchedBox />
+        <MovieDisplayBox>
+          <MovieList movies={movies} />
+        </MovieDisplayBox>
+        <MovieDisplayBox>
+          <WatchedMoviesSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </MovieDisplayBox>
       </MainContent>
+      <StarRating maxRating={5}/>
+      <StarRating maxRating={10}/>
     </>
   );
 }
